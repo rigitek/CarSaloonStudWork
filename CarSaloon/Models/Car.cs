41c9857352ee28c8cc27a6da5c9b2d3c674e21ee
rigitek <rustemmukhametdinov000@gmail.com>
2024-06-05 23:26:11 +0500

    upd

---
+++ CarSaloon/Models/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarSaloon.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public virtual Country Country { get; set; }
+        public virtual Brand Brand { get; set; }
+        public string Model { get; set; }
+        public bool Available { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ CarSaloon/Models/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarSaloon.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Passport { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public bool Delivery { get; set; }
+        public string Paymant { get; set; }
+    }
+}
